29c58e69d135115a241b5b8381fc0d9e0b139f6f
stevenrsewell <stevenrsewell@outlook.com>
2023-12-20 09:56:56 -0500

    Weather Icons

--- weather.js
+++ weather.js
@@ -7,55 +7,78 @@ async function getWeather() {
   weatherInfoDiv.innerHTML = '';
 
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`);
-    const weatherData = await response.json();
-    console.log('Weather Data:', weatherData); // Log the response
+    // Fetch current weather
+    const currentResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=imperial`);
+    const currentWeatherData = await currentResponse.json();
 
     // Display current weather if available
-    if (weatherData.weather && weatherData.weather.length > 0 && weatherData.main) {
-      const currentWeather = document.createElement('p');
-      currentWeather.textContent = `Current Weather: ${weatherData.weather[0].description}, ${weatherData.main.temp}°C`;
-      weatherInfoDiv.appendChild(currentWeather);
+    if (currentWeatherData.weather && currentWeatherData.weather.length > 0 && currentWeatherData.main) {
+      const temperature = Math.round(currentWeatherData.main.temp);
+      const unit = '°F';
+      const sunriseTime = new Date(currentWeatherData.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      const sunsetTime = new Date(currentWeatherData.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+      const temperatureInfo = document.createElement('p');
+      temperatureInfo.textContent = `Current Weather: ${currentWeatherData.weather[0].description}, ${temperature}${unit}<br>Sunrise: ${sunriseTime}, Sunset: ${sunsetTime}`;
+      weatherInfoDiv.appendChild(temperatureInfo);
     } else {
       throw new Error('Invalid data received from the weather API');
     }
 
     // Fetch 5-day forecast
-    const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${apiKey}&units=metric`);
+    const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${apiKey}&units=imperial`);
     const forecastData = await forecastResponse.json();
 
-    // Display daily forecast for the next 5 days with weather icons
-    const dailyForecast = document.createElement('div');
-    dailyForecast.innerHTML = '<h3>5-Day Forecast:</h3>';
-    const dailyData = forecastData.list.filter(entry => entry.dt_txt.includes('12:00:00')); // Retrieve data for 12:00 PM each day
-    dailyData.forEach(day => {
-      const forecast = document.createElement('div');
-
-      // Display date
-      const date = document.createElement('p');
-      date.textContent = new Date(day.dt * 1000).toDateString();
-      forecast.appendChild(date);
-
-      // Display temperature
-      const temperature = document.createElement('p');
-      temperature.textContent = `Temperature: ${day.main.temp}°C`;
-      forecast.appendChild(temperature);
-
-      // Display weather description
-      const weatherDesc = document.createElement('p');
-      weatherDesc.textContent = `Weather: ${day.weather[0].description}`;
-      forecast.appendChild(weatherDesc);
-
-      // Display weather icon based on weather condition code
-      const weatherIcon = document.createElement('img');
-      const iconCode = day.weather[0].icon; // Icon code provided by OpenWeatherMap API
-      weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}.png`; // Image URL based on icon code
-      weatherIcon.alt = day.weather[0].main; // Alt text based on weather status
-      forecast.appendChild(weatherIcon);
-
-      dailyForecast.appendChild(forecast);
+    // Group forecast data by date
+    const dailyForecasts = {};
+    forecastData.list.forEach(entry => {
+      const date = entry.dt_txt.split(' ')[0]; // Extract date (YYYY-MM-DD)
+      if (!dailyForecasts[date]) {
+        dailyForecasts[date] = [];
+      }
+      dailyForecasts[date].push(entry);
     });
-    weatherInfoDiv.appendChild(dailyForecast);
+
+    // Create Bootstrap cards for the next 5 days' forecast
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('row', 'justify-content-start', 'gy-4');
+
+    // Display high and low temperatures for each day
+    Object.keys(dailyForecasts).slice(0, 5).forEach(date => {
+      const dailyData = dailyForecasts[date];
+      const highTemp = Math.round(Math.max(...dailyData.map(entry => entry.main.temp_max)));
+      const lowTemp = Math.round(Math.min(...dailyData.map(entry => entry.main.temp_min)));
+
+      const card = document.createElement('div');
+      card.classList.add('col-12', 'col-md-4');
+
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card', 'text-center', 'h-100');
+
+      const cardTitle = document.createElement('h5');
+      cardTitle.classList.add('card-title');
+      cardTitle.textContent = new Date(dailyData[0].dt * 1000).toLocaleDateString();
+
+      const cardText = document.createElement('p');
+      cardText.classList.add('card-text');
+      cardText.innerHTML = `<strong>Weather:</strong> ${dailyData[0].weather[0].description}<br><strong>High:</strong> ${highTemp}°F<br><strong>Low:</strong> ${lowTemp}°F`;
+
+      const weatherIconContainer = document.createElement('div');
+      weatherIconContainer.classList.add('d-flex', 'justify-content-center');
+
+      const weatherIcon = document.createElement('img');
+      const iconCode = dailyData[0].weather[0].icon;
+      weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
+      
+      weatherIconContainer.appendChild(weatherIcon);
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(weatherIconContainer);
+      cardBody.appendChild(cardText);
+      card.appendChild(cardBody);
+      cardContainer.appendChild(card);
+    });
+
+    weatherInfoDiv.appendChild(cardContainer);
 
   } catch (error) {
     console.error('Error fetching weather data:', error);
